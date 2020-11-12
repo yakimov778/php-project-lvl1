c975d192005362e280174512588fb1af5cf39782
yakimov86 <50272314+yakimov86@users.noreply.github.com>
2020-11-12 22:03:24 +0300

    phpcs

--- src/cli.php
+++ src/cli.php
@@ -9,5 +9,5 @@ function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);;
-}
+    line('Hello, %s!', $name);
+}//end run()
