f44653d3ad7f2cb451ad98acb4d630c82b6745ac
yakimov86 <50272314+yakimov86@users.noreply.github.com>
2020-11-14 21:37:06 +0300

    step 3.0

--- src/cli.php
+++ src/cli.php
@@ -5,9 +5,11 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function run($getGameData, $description)
 {
     line('Welcome to the Brain Game!');
+    line($description);
+    $gameData = $getGameData();
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
 }//end run()
---
+++ src/games/even.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function BrainGames\Cli\run;
+
+const DESCRIPTION = "Answer \"yes\" if the number is even, other answer\"no\".";
+const MAX_RANDOM_VALUE = 99;
+
+function isEven($num)
+{
+    return ($num % 2) === 0;
+}
+
+function runEven()
+{
+    $getEvenData = function () {
+        $question = strval(mt_rand(1, MAX_RANDOM_VALUE));
+        $currentAnswer = isEven($question) ? "yes" : "no";
+        
+        $gameData = [];
+        $gameData['question'] = $question;
+        $gameData['currentAnswer'] = $currentAnswer;
+        return $gameData;
+    };
+    run($getEvenData, DESCRIPTION);
+}
