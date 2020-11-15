ecce0c9e3414b754946d50ca3a0aab1edef1f6f8
yakimov86 <50272314+yakimov86@users.noreply.github.com>
2020-11-15 23:43:14 +0300

    step 5

---
+++ src/games/calc.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BrainGames\Games\Calc;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "What is the result of the expression?";
+const OPERATIONS = ['+', '-', '*'];
+const MAX_RANDOM_VALUE = 99;
+
+function calculate($num1, $num2, $operation)
+{
+    switch ($operation) {
+        case '+':
+            $currentAnswer = $num1 + $num2;
+            break;
+        case '-':
+            $currentAnswer = $num1 - $num2;
+            break;
+        case '*':
+            $currentAnswer = $num1 * $num2;
+            break;
+    }
+    return $currentAnswer;
+}
+
+function runCalc()
+{
+    $getCalcData = function () {
+        $operation = OPERATIONS[array_rand(OPERATIONS)];
+        $num1 = mt_rand(1, MAX_RANDOM_VALUE);
+        $num2 = mt_rand(1, MAX_RANDOM_VALUE);
+        $question = "$num1 $operation $num2";
+        $currentAnswer = strval(calculate($num1, $num2, $operation));
+        
+        $gameData = [];
+        $gameData['currentAnswer'] = $currentAnswer;
+        $gameData['question'] = $question;
+        return $gameData;
+    };
+    run($getCalcData, DESCRIPTION);
+}
