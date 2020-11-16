5822a44b8289ddaefedb90f486f595ccf5b4af04
yakimov86 <50272314+yakimov86@users.noreply.github.com>
2020-11-16 22:53:13 +0300

    step 6

---
+++ src/games/gcd.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+use function BrainGames\Cli\run;
+
+const DESCRIPTION = "Find the greatest common divisor of given numbers.";
+const MAX_RANDOM_VALUE = 99;
+
+function getGcd($num1, $num2)
+{
+    return $num2 ? getGcd($num2, $num1 % $num2) : $num1;
+}
+
+function runGcd()
+{
+    $getGcdData = function () {
+        $num1 = mt_rand(1, MAX_RANDOM_VALUE);
+        $num2 = mt_rand(1, MAX_RANDOM_VALUE);
+        $currentAnswer = strval(getGcd($num1, $num2));
+        $question = "$num1 $num2";
+        
+        $gameData = [];
+        $gameData['currentAnswer'] = $currentAnswer;
+        $gameData['question'] = $question;
+        return $gameData;
+    };
+    run($getGcdData, DESCRIPTION);
+}
